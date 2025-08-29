0fb3052f697bc2d4ef12fa10e665d27c069beda6
OatMealXXII <162794492+OatMealXXII@users.noreply.github.com>
2025-08-29 23:08:13 +0700

    Basic calculator added

---
+++ C++/calculator.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int main() {
+    float a, b;
+    char operation;
+    std::cout << "Enter your type of operation (+, -, *, /) : ";
+    std::cin >> operation;
+    std::cout << "Enter your first number: ";
+    std::cin >> a;
+    std::cout << "Enter your second number: ";
+    std::cin >> b;
+
+    switch (operation)
+    {
+    case '+':
+    std::cout << "Result is: " << a+b << "\n";
+        break;
+    case '-':
+    std::cout << "Result is: " << a-b << "\n";
+    break;
+    case '*':
+    std::cout << "Result is: " << a*b << "\n";
+    break;
+    case '/':
+    std::cout << "Result is: " << a/b << "\n";
+    break;
+    default:
+    std::cout << "You can only use   + - * /   for operation only!\n";
+        break;
+    }
+}
